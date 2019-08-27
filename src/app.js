aead86bbee6e711929a5016f7858cc0b90001615
faiyazbits <m.faiyaz123@gmail.com>
2019-08-27 14:14:30 +0530

    added data for es6 starter

--- src/app.js
+++ src/app.js
@@ -1,8 +1,2 @@
-import {Navabar} from  './data'
-Navabar()
-const greeting = 'Hello World';
-console.log(greeting);
-
-const header = document.getElementById("header");
-header.style.color = "red";
-
+import data from "./data";
+console.log(data);
