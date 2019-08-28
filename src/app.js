771de16b08fbe1717f30369e3d7b57ff8b0574e3
faiyazbits <m.faiyaz123@gmail.com>
2019-08-28 16:11:51 +0530

    added table rendering

--- src/app.js
+++ src/app.js
@@ -1,2 +1,26 @@
 import data from "./data";
-console.log(data);
+
+const root = document.getElementById("root");
+
+function renderRow(parent, keys, map) {
+  const tr = document.createElement("tr");
+  for (let key of keys) {
+    const td = document.createElement("td");
+    td.textContent = map[key] || "no-data";
+    tr.appendChild(td);
+  }
+  parent.appendChild(tr);
+}
+
+function renderTable(parent, data) {
+  const keys = Object.keys(data[0]);
+  const table = document.createElement("table");
+  const tbody = document.createElement("tbody");
+  for (let datum of data) {
+    renderRow(tbody, keys, datum);
+  }
+  table.appendChild(tbody);
+  parent.appendChild(table);
+}
+
+renderTable(root, data);
